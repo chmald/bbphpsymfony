171521c213ec47baaea20a1e508a6ef2c57be996
Christopher Maldonado <chmald@microsoft.com>
2021-03-22 15:53:21 -0500

    updates

--- src/Pagination/Paginator.php
+++ src/Pagination/Paginator.php
@@ -26,7 +26,7 @@ class Paginator
      *
      * See https://symfony.com/doc/current/best_practices.html#use-constants-to-define-options-that-rarely-change
      */
-    public const PAGE_SIZE = 10;
+    public const PAGE_SIZE = 15;
 
     private $queryBuilder;
     private $currentPage;
